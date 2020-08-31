f912b623069beaf81a3cddb107ecb4a3ee0f0931
Cedric Murairi <murairicedric@gmail.com>
2020-08-31 21:22:52 +0200

    added csv_converter to read from a file and copy over to db

--- csv_converter.py
+++ csv_converter.py
@@ -1,23 +1,21 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
 import csv
-
-engine = create_engine('postgresql:///cedricmurairi')
-db = scoped_session(sessionmaker(bind=engine))
+from main import db, Book 
 
 def main():
+	try:
+		with open('/home/cedricmurairi/Documents/CS50Course/projects/project1/books.csv', newline='') as csvfile:
 
-	with open('/home/cedricmurairi/Documents/CS50Course/projects/project1/books.csv', newline='') as csvfile:
-		bookreader = csv.reader(csvfile, delimiter=',', quotechar="'", doublequote=True, skipinitialspace=True)
-		for bookinfo in bookreader:
-			# print(bookinfo[0],'**',bookinfo[1],'**',bookinfo[2],'**',bookinfo[3])
-			# print(', '.join(bookinfo))
-			db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :auth, :year)", {'isbn':bookinfo[0], 'title':bookinfo[1], 'auth':bookinfo[2], 'year':bookinfo[3]})
-		db.commit()
-		# result = db.execute('SELECT * FROM students')
-		# for value in result:
-		# 	print(value.name)
-			# TODO implement the transfer of this data from csv into actual database
+			bookreader = csv.reader(csvfile, delimiter=',', quotechar="'", doublequote=True, skipinitialspace=True)
+
+			for bookinfo in bookreader:
+
+				book = Book(isbn=bookinfo[0], title=bookinfo[1], author=bookinfo[2], year=bookinfo[3])
+				db.session.add(book)
+				
+			db.session.commit()
+	except Exception as e:
+		print(e)
+		return
 
 if __name__ == "__main__":
 	main()
