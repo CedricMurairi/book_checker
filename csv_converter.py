b27e21171f620e5a86d57e3d9c37d24f7ac25ec7
Cedric Murairi <murairicedric@gmail.com>
2020-09-01 12:43:27 +0200

    refactor code in csv file

--- csv_converter.py
+++ csv_converter.py
@@ -5,17 +5,19 @@ def main():
 	try:
 		with open('/home/cedricmurairi/Documents/CS50Course/projects/project1/books.csv', newline='') as csvfile:
 
-			bookreader = csv.reader(csvfile, delimiter=',', quotechar="'", doublequote=True, skipinitialspace=True)
+			bookreader = csv.reader(csvfile, delimiter=',', quotechar='"', doublequote=True, skipinitialspace=True)
 
 			for bookinfo in bookreader:
 
-				book = Book(isbn=bookinfo[0], title=bookinfo[1], author=bookinfo[2], year=bookinfo[3])
+				book = Book(isbn=bookinfo[0], title=bookinfo[1], author=bookinfo[2], pub_year=bookinfo[3])
 				db.session.add(book)
-				
+
 			db.session.commit()
+
+		return True
 	except Exception as e:
 		print(e)
-		return
+		return False
 
 if __name__ == "__main__":
 	main()
